361f59197cd72eff1aa1815b120fb228dd64c8d4
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-05 23:22:42 -0400

    changed env added mongodb

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// app 
+const app = express();
+
+// db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}).then(() => console.log('Bookstore DB Connected'));
 
 app.get('/', (req, res) => {
   res.send("Hello from node")
