b6a59cb6481e3436d2db622ed63ec13eb652fe4d
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-07 22:30:15 -0400

    used express-validator to validate user signup

--- controllers/user.js
+++ controllers/user.js
@@ -17,3 +17,4 @@ exports.signup = (req,res) => {
     });
   });
 };
+
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {signup} = require ('../controllers/user.js');
+const {signup} = require('../controllers/user.js');
+const {userSignupValidator} = require('../validator');
 
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, signup);
 
 module.exports = router;
 
