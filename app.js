c80f525776e0378c101a1eb165153dba04cd7966
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-09 14:50:41 -0400

    jumped ahead with source code, installed cors

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator');
-
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
 // import routes
-const authRoutes = require('./routes/auth')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
-// app 
+// app
 const app = express();
 
 // db
-mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useCreateIndex: true
-}).then(() => console.log('Bookstore DB Connected'));
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
+    .then(() => console.log("DB Connected"));
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 
 // routes middleware
-app.use('/api',  authRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`);
 });
