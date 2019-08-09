64a73d393bcb0608bf9fc945f78fecd2254131a0
Stoneburner Software <ssfw@Seans-MacBook-Pro.local>
2019-08-09 13:02:24 -0400

    Added requireSignin middleware

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {signup, signin, signout} = require('../controllers/user.js');
+const {signup, signin, signout, requireSignin} = require('../controllers/user.js');
 const {userSignupValidator} = require('../validator');
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+
+router.get('/hello', requireSignin,  (req, res) => {
+  res.send('hello, there. ');
+});
 
 module.exports = router;
 
